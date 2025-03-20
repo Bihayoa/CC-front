496d60991c0c6c942ec94e0b0aa1f09da1e2edd0
Bihayoa <Bilalov734@gmail.com>
2025-03-20 18:59:09 +0500

    getPosts folder delete

---
+++ src/config/dbCon.config.js
@@ -0,0 +1,8 @@
+const registerURL = 'http://192.168.108.173:8080/auth/register'
+const loginURL = 'http://192.168.108.173:8080/auth/login'
+const postsURL = 'http://192.168.108.173:8080/posts'
+const usersURL = 'http://192.168.108.173:8080/user'
+const ip = "http://192.168.108.173:8080"
+const putLikeToPost = `${postsURL}/putlike`
+
+export {registerURL, loginURL, postsURL, usersURL, ip, putLikeToPost};
---
+++ src/config/filesPath.config.js
@@ -0,0 +1,3 @@
+const backendUploadDirectoryURL = 'http://192.168.108.173:8080';
+
+export {backendUploadDirectoryURL};
